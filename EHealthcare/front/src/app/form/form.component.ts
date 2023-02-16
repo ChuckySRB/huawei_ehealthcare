34ef6d5e6cfd69d5619887aba49affd0e77e909e
Milos531 <losmi02.stojanovic1822@gmail.com>
2023-02-16 18:11:25 +0100

    Done, no css

--- EHealthcare/front/src/app/form/form.component.ts
+++ EHealthcare/front/src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NumberValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -9,4 +10,29 @@ import { Component } from '@angular/core';
 export class FormComponent {
   constructor () {}
   age : number=0;
+  sex : string;
+  education : number=1;
+  income : number=0;
+  marrige : number;
+  work : string;
+  residence : string;
+  highBP : number=0;
+  highChol : number=0;
+  cholTest : number=0;
+  bmi : number=0;
+  glucose : number=0;
+  smoker : string;
+  stroke : number=0;
+  heartDisease : number=0;
+  pysichalActiv : number=0;
+  fruit : number=0;
+  vegetable : number=0;
+  drink : number=0;
+  healthcare : number=0;
+  noMoney : number=0;
+  genHealth : number=0;
+  mentalHealth : number=0;
+  pysichalHealth : number=0;
+  stairs : number=0;
+
 }

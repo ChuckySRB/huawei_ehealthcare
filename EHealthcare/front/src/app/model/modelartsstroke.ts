eaace07d2bc1f983d36f2416f533b41cbb497c16
ChuckySRB <chucky.srb@gmail.com>
2023-02-17 18:25:51 +0100

    App data ready for modeling!

---
+++ EHealthcare/front/src/app/model/modelartsstroke.ts
@@ -0,0 +1,10 @@
+export class ModelArtsStroke{
+    gender: string;
+    age: number;
+    hearth_disease: number;
+    ever_married: string;
+    work_type: string;
+    residence: string;
+    bmi:number;
+    smoking_status: string;
+}
